1803107284fefa98bd65a8b678d7f3598cbc7e90
Jack Tapping <jacktapping1302@gmail.com>
2020-10-07 13:08:28 +0100

    Added function to chnage the console window size and text color

--- Arithmetic Calculator/Main.cpp
+++ Arithmetic Calculator/Main.cpp
@@ -1,9 +1,72 @@
 #include <IOstream> // Allows the use of cin and cout to get inputs and outputs 
 #include <conio.h> // Allows the use of _getch() to allow the program to wait for user input 
 #include <cmath> // Allows the use of sqrt to to get the squer root of a number 
+#include<Windows.h> // Allows me to change the console window size and text color  
 
 
 //Function 
+
+// changing the colour of the text based on an int paramneter 
+void ChangeTextColour(int iCoulorValue)
+
+//0 = Black 
+//1 = Blue 
+//2 = Green 
+//3 = Aqua 
+//4 = Red 
+//5 = Purple
+//6 = Yellow 
+//7 = White 
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), iCoulorValue);
+}
+
+// changes the size of the consloe windows X and Y cordinates 
+void ChangeConsoleWindowSize(int iXValue, int iYvalue)
+{
+	// changeing the size of the console window 
+	HWND console = GetConsoleWindow();
+	RECT ConsoleRect;
+	GetWindowRect(console, &ConsoleRect);
+
+	MoveWindow(console, ConsoleRect.left, ConsoleRect.top, iXValue, iYvalue, TRUE);
+
+}
+
+// displlaying the intro to the user
+void Intro ()
+{
+	
+	ChangeConsoleWindowSize(400, 150);
+	ChangeTextColour(7);
+
+	std::cout << "##################################################\n\n";
+	ChangeTextColour(1);
+	std::cout << "\tWelcome to the Arithmatic Calculator" << "\n";
+	std::cout << " \t   Press any key to continue..." << "\n" << "\n";
+	ChangeTextColour(7);
+	std::cout << "##################################################";
+
+	_getch();
+	system("cls");
+}
+
+// printing out the main menu to the user 
+void Menu()
+{
+	ChangeConsoleWindowSize(500, 225);
+	//printing out the menu
+	std::cout << "Press 1: for Additon" << "\n";
+	std::cout << "Press 2: for Subtration" << "\n";
+	std::cout << "Press 3: for Divison" << "\n";
+	std::cout << "Press 4: for Multiplication" << "\n";
+	std::cout << "Press 5: for Area of a Rectangle " << "\n";
+	std::cout << "Press 6: for Area of a Triangle " << "\n";
+	std::cout << "Press 7: to Calculate the unknown side of a Right Angle Triangle  " << "\n";
+
+	ChangeTextColour(5);
+	std::cout << "Press E: to Exit the program " << "\n";
+}
 
 // will aske the use for 2 numbers then add them togeather 
 void Addition() {
@@ -128,22 +191,11 @@ void Pythagorean() {
 }
 //Main method is were the computer will look to start the program 
 int main() {
-	std::cout << "Welcome to the Arithmatic Calculator" << "\n";
-	std::cout << "Press any key to continue..." << "\n" << "\n";
 
-	_getch();
-	system("cls"); 
+	Intro();
+	Menu();
 
-
-	//printing out the menu
-	std::cout << "Press 1 for Additon" << "\n";
-	std::cout << "Press 2 for Subtration" << "\n";
-	std::cout << "Press 3 for Divison" << "\n";
-	std::cout << "Press 4 for Multiplication" << "\n";
-	std::cout << "Press 5 for Area of a Rectangle " << "\n";
-	std::cout << "Press 6 for Area of a Triangle " << "\n";
-	std::cout << "Press 7 to Calculate the unknown side of a Right Angle Triangle  " << "\n";
-	std::cout << "Press E to Exit the program " << "\n";
+	
 
 	char cUserInput; //variable to holde the users in put 
 	cUserInput = _getch(); // getting the users input 
